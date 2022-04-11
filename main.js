11da1b60002dc5af3f507eabfafdc39b7ddf1e35
Lucas <lucas.robert@lepoles.org>
2022-04-11 22:34:42 +0200

    site tree.js

---
+++ main.js
@@ -0,0 +1,96 @@
+import './style.css';
+
+import * as THREE from 'three';
+
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls' ;
+
+
+
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+
+renderer.render(scene, camera);
+
+const geometry = new THREE.TorusGeometry (10,3,16,100)
+const material = new THREE.MeshStandardMaterial( { color : 0xFF6347,  } );
+const torys = new THREE.Mesh (geometry, material);
+
+scene.add (torys)
+
+const pointLight = new THREE.PointLight(0xffffff)
+pointLight.position.set (5,5,5)
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight)
+
+const lightHelper = new THREE.PointLightHelper (pointLight)
+const gridHelper = new THREE.GridHelper (200, 50);
+scene.add(lightHelper, gridHelper)
+
+function animate () {
+  requestAnimationFrame (animate);
+
+  torys.rotation.x += 0.01;
+  torys.rotation.y += 0.005;
+  torys.rotation.z += 0.01;
+
+  renderer.render (scene,camera);
+}
+
+animate ()
+
+const controls = new OrbitControls (camera, renderer.domElement);
+
+function addStar () {
+  const geometry = new THREE.SphereGeometry (0.25,24,24);
+  const material = new THREE.MeshStandardMaterial ( { color:0xffffff } )
+  const star = new THREE.Mesh ( geometry,material);
+
+  const [x,y,z] = Array (3).fill().map(() => THREE.MathUtils.randFloatSpread (100) );
+
+  star.position.set (x, y, z);
+  scene.add(star)
+}
+
+Array (200).fill().forEach(addStar)
+
+const spaceTexture = new THREE.TextureLoader ().load('space.jpg');
+scene.background = spaceTexture;
+
+const robyTexture = new THREE.TextureLoader().load('LUCAS LE BG.jpg');
+
+const roby = new THREE.Mesh (
+  new THREE.BoxGeometry(3,3,3),
+  new THREE.MeshBasicMaterial( { map: robyTexture})
+);
+scene.add(roby);
+
+
+
+const moonTexture = new THREE.TextureLoader().load('moon.jpg');
+
+
+const moon = new THREE.Mesh (
+  new THREE.SphereGeometry(3, 32, 32),
+  new THREE.MeshStandardMaterial( {
+    map: moonTexture,
+    
+  } )
+);
+
+scene.add (moon);
+
+
+
+
